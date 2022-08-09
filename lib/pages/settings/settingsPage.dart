d25f76e3c761d8dc9203594ddec1ed38c10fb3b0
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2022-08-09 18:48:46 +0200

    settings page added back button

--- lib/pages/settings/settingsPage.dart
+++ lib/pages/settings/settingsPage.dart
@@ -27,46 +27,72 @@ class settingsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-            child: Column(
-      children: [
-        GestureDetector(
-          onTap: () => {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => changePassword()),
-            )
-          },
-          child: Container(
-              margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
-              child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(children: [
+        body: Container(
+      color: Color(0xFFECEBE7),
+      child: SafeArea(
+          child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                    offset: Offset(2, 2),
+                    color: Color.fromARGB(255, 46, 43, 59))
+              ],
+            ),
+            margin: EdgeInsets.fromLTRB(0, 0, 320, 0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Icon(Icons.arrow_back, size: 20),
+              style: ElevatedButton.styleFrom(
+                  shape: CircleBorder(),
+                  padding: EdgeInsets.all(13),
+                  primary: Color(0xFF7A6DA9),
+                  onPrimary: Color.fromARGB(255, 46, 43, 59),
+                  side: BorderSide(width: 1, color: Color(0xFF103A24))),
+            ),
+          ),
+          GestureDetector(
+            onTap: () => {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => changePassword()),
+              )
+            },
+            child: Container(
+                margin: EdgeInsets.fromLTRB(20, 20, 0, 0),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(children: [
+                        Container(
+                            child: Icon(Icons.lock_outline_rounded, size: 30),
+                            margin: EdgeInsets.fromLTRB(0, 0, 10, 0)),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  child: Text(
+                                'Password ändern',
+                                style: TextStyle(fontSize: 18),
+                              )),
+                              Container(
+                                  child: Text(
+                                      'Ändern sie ihr password in der App',
+                                      style: TextStyle(fontSize: 14)))
+                            ]),
+                      ]),
                       Container(
-                          child: Icon(Icons.lock_outline_rounded, size: 30),
-                          margin: EdgeInsets.fromLTRB(0, 0, 10, 0)),
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                                child: Text(
-                              'Password ändern',
-                              style: TextStyle(fontSize: 18),
-                            )),
-                            Container(
-                                child: Text(
-                                    'Ändern sie ihr password in der App',
-                                    style: TextStyle(fontSize: 14)))
-                          ]),
-                    ]),
-                    Container(
-                      margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                      child: Icon(Icons.keyboard_arrow_right, size: 30),
-                    )
-                  ])),
-        )
-      ],
-    )));
+                        margin: EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        child: Icon(Icons.keyboard_arrow_right, size: 30),
+                      )
+                    ])),
+          )
+        ],
+      )),
+    ));
   }
 }
