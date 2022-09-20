73168cf5b370e0cf5e4ea0162ff29b759aa73571
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2022-09-20 08:51:34 +0200

    removed widgets from teacher homepage

--- lib/pages/homepage/teacherHomepage.dart
+++ lib/pages/homepage/teacherHomepage.dart
@@ -1,15 +1,52 @@
 import 'package:flutter/material.dart';
+import 'widgets/sideMenu.dart';
+import '../../user_data_cache.dart';
+import '../../codeScanner/qrCodeWidget.dart';
+import 'booksListView.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'classList/classMemberCard.dart';
+import 'classList/homepageClassView.dart';
 
-class teacherHomepage extends StatefulWidget {
+class teacherHomepage extends StatelessWidget {
   const teacherHomepage({Key? key}) : super(key: key);
 
   @override
-  State<teacherHomepage> createState() => _teacherHomepageState();
-}
-
-class _teacherHomepageState extends State<teacherHomepage> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Container(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: SafeArea(
+                child: Column(children: [
+              Container(
+                child: Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    boxShadow: [
+                      BoxShadow(offset: Offset(3, 3), color: Color(0xFF18191F))
+                    ],
+                  ),
+                  margin: const EdgeInsets.fromLTRB(320, 7, 7, 0),
+                  child: ElevatedButton(
+                    onPressed: () => Scaffold.of(context).openDrawer(),
+                    style: ElevatedButton.styleFrom(
+                        shape: const CircleBorder(),
+                        padding: const EdgeInsets.all(13),
+                        primary: Colors.white,
+                        onPrimary: Color(0xFF18191F),
+                        side: BorderSide(width: 2, color: Color(0xFF18191F))),
+                    child: FaIcon(FontAwesomeIcons.user,
+                        size: 25, color: Color(0xFF18191F)),
+                  ),
+                ),
+              ),
+              Container(
+                  margin: EdgeInsets.fromLTRB(25, 0, 0, 0),
+                  child: homepageClassView()),
+            ])),
+          )),
+    );
   }
 }
