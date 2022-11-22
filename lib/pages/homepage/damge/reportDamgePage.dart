f158bfbbe4d1dd5351aa9ca107f670bdd1b11297
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2022-11-22 16:19:59 +0100

    created report damage file

---
+++ lib/pages/homepage/damge/reportDamgePage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class reportDamagePage extends StatelessWidget {
+  const reportDamagePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
